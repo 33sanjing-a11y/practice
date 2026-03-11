64b10ddd574729183d7c83455a479ebd8175872f
三景 <33sanjing@gmail.com>
2026-03-11 21:58:18 +0800

    新

--- day1/day3.py
+++ day1/day3.py
@@ -1,1 +1,26 @@
-print("真的吗")
+# import sys
+# def write_hello(file_path):
+#     file = open(file_path, 'w+',encoding='utf-8')
+#     file.write("Hello World")
+#     file.close()
+# if __name__ == '__main__':
+#     write_hello(sys.argv[1])
+# def use_create():
+#     f = open("learning question","w+",encoding="utf-8")
+#     f.write("今天天气真好\n")
+#     f.write("可以一起约会吗")
+#     f.close()
+# if __name__ == '__main__':
+#     use_create()
+import re
+def my_cmp():
+    names = ["name1","_name","2_name","__name__"]
+    for name in names:
+        ret = re.match("[a-zA-Z_]",name)
+        if ret:
+            print(f'{name}是正确的')
+        else:
+            print(f'{name}是错误的')
+
+if __name__ == '__main__':
+    my_cmp()
